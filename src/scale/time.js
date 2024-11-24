c4031767e7eb49da4bb93bec9f45d5454fe94b64
mega16 <mega16@aliyun.com>
2024-11-24 12:38:53 +0800

    feat: add scale

---
+++ src/scale/time.js
@@ -0,0 +1,14 @@
+import { createLinear } from './linear';
+
+export function createTime({ domain, ...rest }) {
+  const transform = (x) => x.getTime();
+  const transformDomain = domain.map(transform);
+  const linear = createLinear({ domain: transformDomain, ...rest });
+
+  const scale = (x) => linear(transform(x));
+
+  scale.nice = (tickCount) => linear.nice(tickCount);
+  scale.ticks = (tickCount) => linear.ticks(tickCount).map((d) => new Date(d));
+
+  return scale;
+}
